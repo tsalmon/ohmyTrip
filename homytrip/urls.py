e6348c73d8c5b98f78fd05905e22425f4dd2cf6a
tsalmon <th_s@hotmail.fr>
2015-03-01 18:34:07 +0100

    login, requirements, history, db

--- homytrip/urls.py
+++ homytrip/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^', include('trip.urls')),
+    url(r'^login/', include('login.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
