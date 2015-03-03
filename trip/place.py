dbcd217b6117aeadb646f0f60528dd3d8f374727
tsalmon <th_s@hotmail.fr>
2015-03-04 00:49:19 +0100

    add __str__ method (keywords for a place)

--- trip/place.py
+++ trip/place.py
@@ -13,38 +13,68 @@ class Bar(Point):
 	def __init__(self, weight_user):
 		super(Bar, self).__init__(weight_user)
 
+	def __str__(self):
+		return "bar"
+
 class Museum(Point):
 	def __init__(self, weight_user):
 		super(Museum, self).__init__(weight_user)
+
+	def __str__(self):
+		return "musem"
 
 class Park(Point):
 	def __init__(self, weight_user):
 		super(Park, self).__init__(weight_user)
 
+	def __str__(self):
+		return "park"
+
 class Beach(Point):
 	def __init__(self, weight_user):
 		super(Beach, self).__init__(weight_user)
+
+	def __str__(self):
+		return "beach"
 
 class SkyStation(Point):
 	def __init__(self, weight_user):
 		super(SkyStation, self).__init__(weight_user)
 
+	def __str__(self):
+		return "sky"
+
 class Restaurant(Point):
 	def __init__(self, weight_user):
 		super(Restaurant, self).__init__(weight_user)
+
+	def __str__(self):
+		return "restaurant"
 
 class NightClub(Point):
 	def __init__(self, weight_user):
 		super(NightClub, self).__init__(weight_user)
 
+	def __str__(self):
+	return "boite de nuit"
+
 class Zoo(Point):
 	def __init__(self, weight_user):
 		super(Zoo, self).__init__(weight_user)
+
+	def __str__(self):
+		return "zoo"
 
 class Bridge(Point):
 	def __init__(self, weight_user):
 		super(Bridge, self).__init__(weight_user)
 
+	def __str__(self):
+		return "pont"
+
 class Board(Point):
 	def __init__(self, weight_user):
 		super(Board, self).__init__(weight_user)
+
+	def __str__(self):
+		return "port"
