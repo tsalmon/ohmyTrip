2450fb1d08b5fce2d5d4c482217382845705e2b0
tsalmon <th_s@hotmail.fr>
2015-03-04 01:21:21 +0100

    add yelp_helper (can now make api request to yelp

--- trip/models.py
+++ trip/models.py
@@ -1,19 +1,7 @@
 from django.db import models
-
-class Sejour(object):
-	id_sejour = -1
-
-	def __init__(self, date_begin, date_end, places, user):
-		Sejour.id_sejour += 1
-		self.date_begin = date_begin
-		self.date_end = date_end
-		self.places = places
-		self.user = user
-		self.id = Sejour.id_sejour
-		print "create sejour (%d)" % self.id
-
-	def getId(self):
-		return self.id
+from place import *
+from sejour import Sejour
+from pprint import pprint as var_dump
 
 class Factory(object):
 	#TODO: make a get() method.
@@ -55,7 +43,7 @@ class SejourFactory(Factory):
 		sejour = Sejour(date_debut, date_fin, places, user)
 		SejourFactory.sejours[sejour.getId()] = sejour
 		return sejour
-	
+
 class User(models.Model):
 	id_user = -1
 	name = models.CharField(max_length=200)
@@ -69,71 +57,24 @@ class User(models.Model):
 		self.password = password
 		self.profil = profil
 		self.id = User.id_user
-		self.profil = Profil(profil)
+		self.profil = profil
 		print "create user (%d)" % self.id
 
 	def getId(self):
 		return self.id
 
-class Profil(object):
+	def getProfil(self):
+		return self.profil
 
+	def getInteret(self, place):
+		return self.profil[place]
+
+class Profil(object):
+	#TODO: make a class which abstract profil of a user
 	def __init__(self, profil_user):
 		self.profil = {}
 
 		for item in profil_user:
-			print "%s: %d" % (item, profil_user[item])
-			self.profil[item] = eval(item)(profil_user[item])
+			self.profil[item] = item(profil_user[item])
 
 
-class Place(object):
-	
-	def __init__(self, country, local):
-		pass
-
-class Region(Place):
-	points = [] # Point list
-
-class Point(Place):
-	def __init__(self, weight_user):
-		self.weight_user = weight_user
-
-class Bar(Point):
-	def __init__(self, weight_user):
-		super(Bar, self).__init__(weight_user)
-
-class Museum(Point):
-	def __init__(self, weight_user):
-		super(Museum, self).__init__(weight_user)
-
-class Park(Point):
-
-	def __init__(self, weight_user):
-		super(Park, self).__init__(weight_user)
-
-class Beach(Point):
-	def __init__(self, weight_user):
-		super(Beach, self).__init__(weight_user)
-
-class SkyStation(Point):
-	def __init__(self, weight_user):
-		super(SkyStation, self).__init__(weight_user)
-
-class Restaurant(Point):
-	def __init__(self, weight_user):
-		super(Restaurant, self).__init__(weight_user)
-
-class NightClub(Point):
-	def __init__(self, weight_user):
-		super(NightClub, self).__init__(weight_user)
-
-class Zoo(Point):
-	def __init__(self, weight_user):
-		super(Zoo, self).__init__(weight_user)
-
-class Bridge(Point):
-	def __init__(self, weight_user):
-		super(Bridge, self).__init__(weight_user)
-
-class Board(Point):
-	def __init__(self, weight_user):
-		super(Board, self).__init__(weight_user)
