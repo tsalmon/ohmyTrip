0403952fc2ffaaaf24a18b38ad02d8df401a3052
tsalmon <th_s@hotmail.fr>
2015-03-06 16:17:34 +0100

    add coverage (django) to the project

--- homytrip/settings.py
+++ homytrip/settings.py
@@ -70,6 +70,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+TEST_RUNNER = 'django_coverage.coverage_runner.CoverageRunner'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
