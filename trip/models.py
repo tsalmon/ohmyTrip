fb9f05671f73779d4016139245d33e777169b97b
tsalmon <th_s@hotmail.fr>
2015-03-06 12:43:11 +0100

    yelp search work

--- trip/models.py
+++ trip/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from place import *
 from sejour import Sejour
+from user import User
 from pprint import pprint as var_dump
 
 class Factory(object):
@@ -43,38 +44,4 @@ class SejourFactory(Factory):
 		sejour = Sejour(date_debut, date_fin, places, user)
 		SejourFactory.sejours[sejour.getId()] = sejour
 		return sejour
-
-class User(models.Model):
-	id_user = -1
-	name = models.CharField(max_length=200)
-	mail = models.EmailField(max_length=200)
-
-	def __init__(self, firstname, lastname, mail, password, profil):
-		User.id_user += 1
-		self.firstname = firstname
-		self.lastname = lastname
-		self.mail = mail
-		self.password = password
-		self.profil = profil
-		self.id = User.id_user
-		self.profil = profil
-		print "create user (%d)" % self.id
-
-	def getId(self):
-		return self.id
-
-	def getProfil(self):
-		return self.profil
-
-	def getInteret(self, place):
-		return self.profil[place]
-
-class Profil(object):
-	#TODO: make a class which abstract profil of a user
-	def __init__(self, profil_user):
-		self.profil = {}
-
-		for item in profil_user:
-			self.profil[item] = item(profil_user[item])
-
 
