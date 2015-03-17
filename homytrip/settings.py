ebb9ffa21cea92c023719c1751a342077f39e41b
tsalmon <th_s@hotmail.fr>
2015-03-17 22:10:49 +0100

    add register to homytrip's apps

--- homytrip/settings.py
+++ homytrip/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login',
+    'register',
     'trip',
 )
 
--- homytrip/urls.py
+++ homytrip/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^', include('trip.urls')),
+    url(r'^register/', include('register.urls')),
     url(r'^login/', include('login.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('trip.urls')),
 )
