a5ffdb8d584f591596470acf77cf97bd09e7649a
tsalmon <th_s@hotmail.fr>
2015-03-17 19:26:45 +0100

    html for profil

--- register/views.py
+++ register/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
+from django.template import Template, Context
 from trip.models import UserFactory
 import pprint
 
 def index(request):
-	register = True;
-	pprint.pprint(request.POST)
+	#pprint.pprint(request.POST)
 	firstname = request.POST['firstname'] 
 	lastname = request.POST['lastname']
 	mail = request.POST['mail']
 	password = request.POST["password"]
 	profil = {}
 	user = UserFactory.create_user(firstname, lastname, mail, password, profil)
-	return render(request, 'home.html', { 'register': register })
+	#pprint.pprint(user.getId())
+	profil_choices = [ "bar", "Museum", "Park", "Beach", "Skystation", "Restaurant", "NightClub", "Zoo", "Bridge", "Board", "Church", "Landmarks"]
+	return render(request, 'home.html', { 'register': True, "profil" : profil_choices, })
