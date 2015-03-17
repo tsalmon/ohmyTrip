09144073f9219b409a04d9b6e67a3293179294b5
tsalmon <th_s@hotmail.fr>
2015-03-17 23:39:08 +0100

    can now log in on the site

--- connexion/views.py
+++ connexion/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.core import serializers
+from trip.user import User
 
-# Create your views here.
+def index(request):
+	user = User.objects.get(mail=request.POST['mail'])
+	request.session["user"] = user.id
+	return render(request, 'home.html', {"connect" : True})
