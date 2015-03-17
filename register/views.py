6a4a3868269877bde1b978509cdfe661c2ef981e
tsalmon <th_s@hotmail.fr>
2015-03-17 11:18:18 +0100

    register : create-user -> create-profil transition

--- register/views.py
+++ register/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from trip.user import User
+from trip.models import UserFactory
 import pprint
 
 def index(request):
 	register = True;
 	pprint.pprint(request.POST)
+	firstname = request.POST['firstname'] 
+	lastname = request.POST['lastname']
+	mail = request.POST['mail']
+	password = request.POST["password"]
+	profil = {}
+	user = UserFactory.create_user(firstname, lastname, mail, password, profil)
 	return render(request, 'home.html', { 'register': register })
